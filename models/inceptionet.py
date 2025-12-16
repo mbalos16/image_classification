59b74a9f63af770c271320b479da632edc64c6ae
Maria Balos <mariabalos16@gmail.com>
2025-12-16 13:09:36 +0000

    Refactor inceptionet code

--- models/inceptionet.py
+++ models/inceptionet.py
@@ -2,6 +2,66 @@ import torch
 import torchvision.transforms as transforms
 
 
+class InceptionBlock(torch.nn.Module):
+    def __init__(
+        self,
+        in_channels=192,
+        out_ch_h1=64,
+        out_ch_h2=96,
+        out_ch_h2_2=128,
+        out_ch_h3=16,
+        out_ch_h3_3=32,
+        out_ch_h4=32,
+        kernel_size=1,
+    ):
+        super(InceptionBlock, self).__init__()
+        self.conv_1 = torch.nn.Conv2d(
+            in_channels=in_channels, out_channels=out_ch_h1, kernel_size=kernel_size
+        )  # Col 1
+
+        self.conv_2 = torch.nn.Conv2d(
+            in_channels=in_channels, out_channels=out_ch_h2, kernel_size=kernel_size
+        )  # Col 2
+        self.conv_3 = torch.nn.Conv2d(
+            in_channels=out_ch_h2, out_channels=out_ch_h2_2, kernel_size=3, padding=1
+        )
+
+        self.conv_4 = torch.nn.Conv2d(
+            in_channels=in_channels, out_channels=out_ch_h3, kernel_size=kernel_size
+        )  # Col 3
+        self.conv_5 = torch.nn.Conv2d(
+            in_channels=out_ch_h3, out_channels=out_ch_h3_3, kernel_size=5, padding=2
+        )
+
+        self.max_pool = torch.nn.MaxPool2d(kernel_size=3, stride=1, padding=1)  # Col 4
+        self.conv_6 = torch.nn.Conv2d(
+            in_channels=in_channels, out_channels=out_ch_h4, kernel_size=kernel_size
+        )
+
+        self.activation = torch.nn.ReLU()
+
+    def forward(self, x):
+        # Initial shape: (8, 3, 128, 128)
+        h1 = self.conv_1(x)  # COL 1
+        h1 = self.activation(h1)
+
+        h2 = self.conv_2(x)  # COL 2
+        h2 = self.activation(h2)
+        h2 = self.conv_3(h2)
+        h2 = self.activation(h2)  # Shape: 8, 128, 128, 128
+
+        h3 = self.conv_4(x)  # COL 3
+        h3 = self.activation(h3)
+        h3 = self.conv_5(h3)
+        h3 = self.activation(h3)  # Shape: 8, 32, 128, 128
+
+        h4 = self.max_pool(x)  # COL 4
+        h4 = self.conv_6(h4)
+        h4 = self.activation(h4)  # Shape: 8, 32, 128, 128
+
+        return torch.cat((h1, h2, h3, h4), dim=1)
+
+
 class GoogLeNet(torch.nn.Module):
     def __init__(self, dropout=0.4):
         super(GoogLeNet, self).__init__()
@@ -23,65 +83,39 @@ class GoogLeNet(torch.nn.Module):
         # 3x3 MaxPool same as stage 1
 
         # Stage Three | Inception BlocK One
-        # Col 1
-        self.ins_conv_1x1_64 = torch.nn.Conv2d(
-            in_channels=192, out_channels=64, kernel_size=1
-        )
-        # Col 2
-        self.ins_conv_1x1_96 = torch.nn.Conv2d(
-            in_channels=192, out_channels=96, kernel_size=1
-        )
-        self.ins_conv_3x3_128 = torch.nn.Conv2d(
-            in_channels=96, out_channels=128, kernel_size=3, padding=1
-        )
-        # Col 3
-        self.ins_conv_1x1_16 = torch.nn.Conv2d(
-            in_channels=192, out_channels=16, kernel_size=1
-        )
-        self.ins_conv_5x5_32 = torch.nn.Conv2d(
-            in_channels=16, out_channels=32, kernel_size=5, padding=2
-        )
-        # Col 4
-        self.ins_max_pool = torch.nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
-        self.ins_conv_1x1_32 = torch.nn.Conv2d(
-            in_channels=192, out_channels=32, kernel_size=1
+        self.block_one = InceptionBlock(
+            in_channels=192,
+            out_ch_h1=64,
+            out_ch_h2=96,
+            out_ch_h2_2=128,
+            out_ch_h3=16,
+            out_ch_h3_3=32,
+            out_ch_h4=32,
+            kernel_size=1,
         )
 
-        # Stage Three | Inception BlocK Two
-        # Col 1
-        self.ins2_conv_1x1_128 = torch.nn.Conv2d(
-            in_channels=256, out_channels=128, kernel_size=1
+        # Stage Three | Inception BlocK One
+        self.block_two = InceptionBlock(
+            in_channels=256,
+            out_ch_h1=128,
+            out_ch_h2=128,
+            out_ch_h2_2=192,
+            out_ch_h3=32,
+            out_ch_h3_3=96,
+            out_ch_h4=64,
+            kernel_size=1,
         )
-        # Col 2
-        # same: ins2_conv_1x1_128
-        self.ins2_conv_3x3_192 = torch.nn.Conv2d(
-            in_channels=128, out_channels=192, kernel_size=3, padding=1
-        )
-        # Col 3
-        self.ins2_conv_1x1_32 = torch.nn.Conv2d(
-            in_channels=256, out_channels=32, kernel_size=1
-        )
-        self.ins2_conv_5x5_96 = torch.nn.Conv2d(
-            in_channels=32, out_channels=96, kernel_size=5, padding=2
-        )
-        # Col 4
-        self.ins2_max_pool = torch.nn.MaxPool2d(kernel_size=3)
-        self.ins2_conv_1x1_64 = torch.nn.Conv2d(
-            in_channels=256, out_channels=64, kernel_size=1
-        )
-        # # TODO CONCATENATION
 
         self.max_pool2 = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
         # Out
         # Global AVg Pool
         self.adaptive_avg_pool = torch.nn.AdaptiveAvgPool2d(output_size=1)
-        self.fully_connected = torch.nn.Linear(in_features=608, out_features=84)
+        self.fully_connected = torch.nn.Linear(in_features=480, out_features=84)
         self.output = torch.nn.Linear(in_features=84, out_features=6)
 
         self.activation = torch.nn.ReLU()
         self.dropout = torch.nn.Dropout(p=dropout)
-        self.softmax = torch.nn.Softmax()
 
     def forward(self, x):
         # Input size: (batch_size, 3, 128, 128)
@@ -103,62 +137,10 @@ class GoogLeNet(torch.nn.Module):
         x = self.max_pool(x)  # Shape: (batch_size, 192, 32, 32)
 
         # Stage Three | Inception Block One
-        #  Conv 1x1, (64)
-        x_i_1 = self.ins_conv_1x1_64(x)  # Shape: (batch_size, 64, 32, 32)
-        x_i_1 = self.activation(x_i_1)
-
-        # Conv 1x1, (96) and Conv 3x3, (128)
-        x_i_2 = self.ins_conv_1x1_96(x)  # Shape: (batch_size, 96, 32, 32)
-        x_i_2 = self.activation(x_i_2)
-        x_i_2 = self.ins_conv_3x3_128(x_i_2)  # Shape: (batch_size, 128, 32, 32)
-        x_i_2 = self.activation(x_i_2)
-
-        # Conv 1x1, (16) and Conv 5x5, (32)
-        x_i_3 = self.ins_conv_1x1_16(x)  # Shape: (batch_size, 16, 32, 32)
-        x_i_3 = self.activation(x_i_3)
-        x_i_3 = self.ins_conv_5x5_32(x_i_3)  # Shape: (batch_size, 32, 32, 32)
-        x_i_3 = self.activation(x_i_3)
-
-        # MaxPool 3x3 and Conv 1x1 (32)
-        x_i_4 = self.ins_max_pool(
-            x
-        )  # Shape: (batch_size, 192, 30, 30) # Why in debbug its (8, 192, 10, 10)
-        x_i_4 = self.ins_conv_1x1_32(x_i_4)  # Shape: (batch_size, 32, 30, 30)
-        x_i_4 = self.activation(x_i_4)
-
-        # Concatenation
-        x = torch.cat(
-            (x_i_1, x_i_2, x_i_3, x_i_4), dim=1
-        )  # Shape: (batch_size, 256, 32, 32)
+        x = self.block_one(x)
 
         # Stage Three | Inception Block Two
-        # Conv 1x1, (128)
-        x_i2_1 = self.ins2_conv_1x1_128(x)  # Shape: (batch_size, 128, 32, 32) # WRONG
-        x_i2_1 = self.activation(x)
-
-        # Conv 1x1, (128) and Conv 3x3, (192)
-        x_i2_2 = self.ins2_conv_1x1_128(x)  # Shape: (batch_size, 128, 32, 32)
-        x_i2_2 = self.activation(x_i2_2)
-        x_i2_2 = self.ins2_conv_3x3_192(x_i2_2)  # Shape: (batch_size, 192, 32, 32)
-        x_i2_2 = self.activation(x_i2_2)
-
-        # Conv 1x1, (32) and Conv 5x5, (96)
-        x_i2_3 = self.ins2_conv_1x1_32(x)  # Shape: (batch_size, 32, 32, 32)
-        x_i2_3 = self.activation(x_i2_3)
-        x_i2_3 = self.ins2_conv_5x5_96(x_i2_3)  # Shape: (batch_size, 96, 32, 32)
-        x_i2_3 = self.activation(x_i2_3)
-
-        # MaxPool 3x3 and Conv 1x1 (64)
-        x_i2_4 = self.ins_max_pool(x)  # Shape: (batch_size, 256, 32, 32)
-        x_i2_4 = self.ins2_conv_1x1_64(
-            x_i2_4
-        )  # Shape: (batch_size, 64, 32, 32) # Falla sin razon.
-        x_i2_4 = self.activation(x_i2_4)
-
-        # Concatenation
-        x = torch.cat(
-            (x_i2_1, x_i2_2, x_i2_3, x_i2_4), dim=1
-        )  # Shape: (batch_size, 608, 32, 32)
+        x = self.block_two(x)
 
         # 3X3MaxPool
         x = self.max_pool2(x)  # Shape: (batch_size, 608, 32, 32)
@@ -174,6 +156,4 @@ class GoogLeNet(torch.nn.Module):
         x = self.dropout(x)
         x = self.output(x)
 
-        # Softmax
-        x = self.softmax(x)
         return x
