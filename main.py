ab20bb54380991cf5a4febbc15918c84c1d9e207
Maria Balos <mariabalos16@gmail.com>
2025-12-18 15:48:33 +0000

    Fix script: add parser, improve model log name.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Import libraries.
+import argparse
 import os
 import random as rd
+import time
 from functools import partial
 
 import kagglehub
@@ -9,83 +11,166 @@ import torch
 import torchvision.transforms as transforms
 from matplotlib import pyplot as plt
 from PIL import Image
+from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchinfo import summary
 from torchvision.datasets import ImageFolder
 
 from models.alexnet import AlexNet
-from models.custom_cnn import CustomModel
+from models.baseline_cnn import BaselineCNN
+from models.densenet import DenseNet
+from models.inceptionet import GoogLeNet
 from models.lenet import LeNet, LeNetMod, LeNetModNorm, LeNetModNorm2
 from models.resnet import ResNet
+from models.vit import Vit
 from trainer import train
 
-# Load the data.
-path = kagglehub.dataset_download("kaustubhdikshit/neu-surface-defect-database")
-validation_path_img = os.path.join(path, "NEU-DET", "validation", "images")
-validation_path_ann = os.path.join(path, "NEU-DET", "validation", "annotations")
-train_path_img = os.path.join(path, "NEU-DET", "train", "images")
-train_path_ann = os.path.join(path, "NEU-DET", "train", "annotations")
+MODEL_LIBRARY = {
+    "baseline_cnn": BaselineCNN,
+    "lenet": LeNet,
+    "lenet_mod": LeNetMod,
+    "lenet_mod_norm": LeNetModNorm,
+    "lenet_mod_norm2": LeNetModNorm2,
+    "alexnet": AlexNet,
+    "inceptionet": GoogLeNet,
+    "resnet": ResNet,
+    "densenet": DenseNet,
+    "vit": Vit,
+}
 
-# Check the diferent categories available in the training dataset and print them in the console.
-training_categories = os.listdir(train_path_img)
-print(
-    f"The training dataset has {len(training_categories)} categories which are: {training_categories}"
-)
 
-# Print 3 images for each category in the dataset
-for category in training_categories:
-    # Plot 3 random images from the training set for each category
-    fig, axes = plt.subplots(1, 3, figsize=(12, 4))  # 1 row, 3 columns
-    for idx in range(3):
-        random_image = rd.randint(1, 240)
+def parse_args():
+    parser = argparse.ArgumentParser(description="An example script")
 
-        # Plot the image
-        image_path = os.path.join(
-            train_path_img, category, f"{category}_{random_image}.jpg"
-        )
-        with Image.open(image_path) as im:
-            image = im.convert("RGB")
-        axes[idx].imshow(image)
-        axes[idx].set_title(f"{category}_{random_image}", loc="left")
-
-    plt.suptitle(f"Category: {category}\n", fontsize=14)
-    plt.tight_layout()
-    plt.show()
-
-    # Check if all images in the category have the same dimensions
-    cat_len = len(os.listdir(os.path.join(train_path_img, category)))
-    initia_width, initial_height = 0, 0
-    for i in range(cat_len):
-        image_path = os.path.join(train_path_img, category, f"{category}_{i+1}.jpg")
-        with Image.open(image_path) as im:
-            width, height = im.size
-            if i == 0:
-                initia_width = width
-                initial_height = height
-            else:
-                if width != initia_width or height != initial_height:
-                    print(
-                        f"Image {i+1} in category {category} has different dimensions: {width}x{height} vs {initia_width}x{initial_height}"
-                    )
-    print(
-        f"Image dimmensions: {initia_width} x {initial_height} in category {category}"
+    parser.add_argument(
+        "-m",
+        "--model_name",
+        required=True,
+        type=str,
+        help="The name of the model you want to train.",
     )
-    print(f"The category {category} in training has {cat_len} images.\n\n")
 
-# Check the different categories in the validation dataset
-validation_categories = os.listdir(validation_path_img)
-print(
-    f"The validation dataset has {len(validation_categories)} categories which are: {validation_categories}"
-)
-print(
-    f"The validation dataset has: {len(validation_categories) * len(os.listdir(os.path.join(validation_path_img, category)))} images"
-)
+    parser.add_argument(
+        "-e",
+        "--epocs",
+        type=int,
+        required=False,
+        default=2000,
+        help="Number of epocs to train the model.",
+    )
 
-# Data transformation and one hot encoding
-transform = transforms.Compose(
-    [
-        transforms.Resize((128, 128)),
-        transforms.ToTensor(),
-        # transforms.RandomHorizontalFlip(p=0.5)
-    ]
-)
+    parser.add_argument(
+        "-lr",
+        "--learning_rate",
+        type=float,
+        required=False,
+        default=0.00001,
+        help="The learning rate used for the optimizer.",
+    )
+
+    parser.add_argument(
+        "-wp",
+        "--warmup_period",
+        type=int,
+        required=False,
+        default=2000,
+        help="The warmup period needed to warm up the learning rate. Makes hyperparameter tuning more robust while improving the final performance.",
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def one_hot_encoding_labels(label, num_classes):
+    """Transform the labels in one_hot_encoding."""
+    one_hot_encoding_labels = torch.nn.functional.one_hot(
+        torch.tensor(label),
+        num_classes=num_classes,
+    )
+    return one_hot_encoding_labels
+
+
+def get_dataloaders():
+    # Load the data.
+    path = kagglehub.dataset_download("kaustubhdikshit/neu-surface-defect-database")
+    validation_path_img = os.path.join(path, "NEU-DET", "validation", "images")
+    validation_path_ann = os.path.join(path, "NEU-DET", "validation", "annotations")
+    train_path_img = os.path.join(path, "NEU-DET", "train", "images")
+    train_path_ann = os.path.join(path, "NEU-DET", "train", "annotations")
+
+    # Check the diferent categories available in the training dataset and print them in the console.
+    training_categories = os.listdir(train_path_img)
+    print(
+        f"The training dataset has {len(training_categories)} categories which are: {training_categories}"
+    )
+
+    # Data transformation and one hot encoding
+    transform = transforms.Compose(
+        [
+            transforms.Resize((128, 128)),
+            transforms.ToTensor(),
+            # transforms.RandomHorizontalFlip(p=0.5)
+        ]
+    )
+
+    train_dataset = ImageFolder(
+        root=train_path_img,  # Path to the dataset root directory
+        transform=transform,  # Apply the defined transformations
+        target_transform=partial(one_hot_encoding_labels, num_classes=6),
+    )
+
+    validation_dataset = ImageFolder(
+        root=validation_path_img,  # Path to the dataset root directory
+        transform=transform,  # Apply the defined transformations
+        target_transform=partial(one_hot_encoding_labels, num_classes=6),
+    )
+
+    # A dictionary is defined to eazily switch between training and validation datasets during training.
+    dataloaders = {
+        "train": DataLoader(train_dataset, batch_size=8, shuffle=True),
+        "val": DataLoader(validation_dataset, batch_size=8, shuffle=False),
+    }
+
+    return dataloaders
+
+
+def main():
+    args = parse_args()
+
+    # Instantiate the model
+    if not args.model_name in MODEL_LIBRARY:
+        raise ValueError(
+            f"The model {args.model_name} is not available. Please choose one of {MODEL_LIBRARY.keys()}."
+        )
+    model = MODEL_LIBRARY[args.model_name]()
+
+    dataloaders = get_dataloaders()
+    epochs = args.epocs
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    warmup_scheduler = warmup.LinearWarmup(optimizer, args.warmup_period)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model.to(device)
+
+    # Define the name to be used for the logdir folder for each model
+    year = time.gmtime().tm_year
+    month = time.gmtime().tm_mon
+    day = time.gmtime().tm_mday
+    model_log_name = f"{year}_{month}_{day}_{args.model_name}"
+    writer = SummaryWriter(
+        os.path.join("runs", model_log_name)
+    )  # Tensor Board Logs Class
+
+    train(
+        epochs=epochs,
+        dataloaders=dataloaders,
+        optimizer=optimizer,
+        device=device,
+        model=model,
+        writer=writer,
+        warmup_scheduler=warmup_scheduler,
+    )
+
+
+if __name__ == "__main__":
+    main()
